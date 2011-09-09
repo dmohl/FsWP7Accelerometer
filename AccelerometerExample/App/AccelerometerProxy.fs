6056e1cb8ac3162cc1529470a70c77abee96d63a
= <dmohl@yahoo.com>
2011-09-09 09:48:38 -0500

    Created

---
+++ AccelerometerExample/App/AccelerometerProxy.fs
@@ -0,0 +1,74 @@
+﻿module AccelerometerProxy
+
+open System
+open System.Windows
+open Microsoft.Devices
+open Microsoft.Devices.Sensors
+
+type AccelerometerEventArgs() =
+    inherit EventArgs()
+    let mutable x = 0.0
+    let mutable y = 0.0
+    let mutable z = 0.0
+    let mutable timestamp = DateTimeOffset(DateTime.Now)
+    member this.X with get() = x and set v = x <- v
+    member this.Y with get() = y and set v = y <- v
+    member this.Z with get() = z and set v = z <- v
+    member this.Timestamp with get() = timestamp and set v = timestamp <- v
+
+type DeviceAccelerometer(startFunc, stopFunc) = 
+    let readingChangedEvent = Event<EventHandler<AccelerometerEventArgs>, AccelerometerEventArgs>()
+    member x.Start () = readingChangedEvent |> startFunc x
+    member x.Stop () = stopFunc()
+    member x.ReadingChanged = readingChangedEvent.Publish
+    member x.OnReadingChanged eventArgs = readingChangedEvent.Trigger(x, eventArgs)
+
+let DefaultAccelerometer() = 
+    let accelerometer = new Accelerometer()
+    
+    let startFunc x event = 
+        (fun (x, event:Event<EventHandler<AccelerometerEventArgs>, AccelerometerEventArgs>) -> 
+            accelerometer.ReadingChanged.Add(fun e -> event.Trigger(x, 
+                                                          AccelerometerEventArgs(X=e.X, Y=e.Y, Z=e.Z, 
+                                                              Timestamp=e.Timestamp)))
+            do accelerometer.Start()
+        )(x, event)
+
+    let stopFunc () = (fun() -> accelerometer.Stop())()
+    (startFunc, stopFunc)
+
+let MouseAccelerometer() = 
+    let mouseDownLocation = ref (new Point())
+    let isMouseDown = ref false
+    let tolerance = 0.001
+    let target = Application.Current.RootVisual :?> FrameworkElement
+    
+    let startFunc x event = 
+        (fun (x, event:Event<EventHandler<AccelerometerEventArgs>, AccelerometerEventArgs>) -> 
+            target.MouseLeftButtonDown.Add(fun e -> if (target.CaptureMouse()) then
+                                                        isMouseDown := true
+                                                        mouseDownLocation := e.GetPosition(target))
+
+            target.MouseMove.Add(fun e -> if !isMouseDown then
+                                              let position = e.GetPosition target
+                                              let x = (position.X - (!mouseDownLocation).X) * tolerance
+                                              let y = -(position.Y - (!mouseDownLocation).Y) * tolerance
+                                              let timestamp = new DateTimeOffset(DateTime.Now)
+                                              event.Trigger(x, 
+                                                  new AccelerometerEventArgs(X=x, Y=y, Z=0.0, Timestamp=timestamp)) )
+
+            target.MouseLeftButtonUp.Add(fun e -> target.ReleaseMouseCapture())
+
+            target.LostMouseCapture.Add(fun e -> isMouseDown := false)
+        )(x, event)
+
+    let stopFunc () = if (!isMouseDown) then
+                          target.ReleaseMouseCapture()
+                          isMouseDown := false
+    (startFunc, stopFunc)
+
+let GetAccelerometer() =
+    let (startFunc, stopFunc) = match Microsoft.Devices.Environment.DeviceType with
+                                | DeviceType.Device -> DefaultAccelerometer()
+                                | _ -> MouseAccelerometer()
+    DeviceAccelerometer(startFunc, stopFunc)
---
+++ AccelerometerExample/App/AppLogic.fs
@@ -0,0 +1,89 @@
+﻿namespace WindowsPhoneApp
+
+open System
+open System.Net
+open System.Windows
+open System.Windows.Controls
+open System.Windows.Documents
+open System.Windows.Ink
+open System.Windows.Input
+open System.Windows.Media
+open System.Windows.Media.Animation
+open System.Windows.Shapes
+open System.Windows.Navigation
+open Microsoft.Phone.Controls
+open Microsoft.Phone.Shell
+open AccelerometerProxy
+
+#nowarn "40"
+[<AutoOpen>]
+module private Utilities = 
+    /// This is an implementation of the dynamic lookup operator for binding
+    /// Xaml objects by name.
+    let (?) (source:obj) (s:string) =
+        match source with 
+        | :? ResourceDictionary as r ->  r.[s] :?> 'T
+        | :? Control as source -> 
+            match source.FindName(s) with 
+            | null -> invalidOp (sprintf "dynamic lookup of Xaml component %s failed" s)
+            | :? 'T as x -> x
+            | _ -> invalidOp (sprintf "dynamic lookup of Xaml component %s failed because the component found was of type %A instead of type %A"  s (s.GetType()) typeof<'T>)
+        | _ -> invalidOp (sprintf "dynamic lookup of Xaml component %s failed because the source object was of type %A. It must be a control or a resource dictionary" s (source.GetType()))
+
+    let accelerometer = AccelerometerProxy.GetAccelerometer()
+
+/// This type implements the main page of the application
+type MainPage() as this =
+    inherit PhoneApplicationPage()
+
+    // Load the Xaml for the page.
+    do Application.LoadComponent(this, new System.Uri("/WindowsPhoneApp;component/MainPage.xaml", System.UriKind.Relative))
+    let contentTextBlock : TextBlock = this?txtContent
+
+    let ProcessAccelerometerData (x:double) (y:double) (z:double) (timestamp:DateTimeOffset) =
+        let content = sprintf "X = %f\n Y = %f\n Z=%f\n Magnitude = %f\n\n %O" x y z (sqrt(x * x + y * y + z * z)) timestamp
+        this.Dispatcher.BeginInvoke(fun _ -> contentTextBlock.Text <- content) |> ignore
+
+    do accelerometer.ReadingChanged
+       |> Event.add ( fun (args:AccelerometerEventArgs) -> 
+                          ProcessAccelerometerData args.X args.Y args.Z args.Timestamp )
+
+/// One instance of this type is created in the application host project.
+type App(app:Application) = 
+    // Global handler for uncaught exceptions. 
+    // Note that exceptions thrown by ApplicationBarItem.Click will not get caught here.
+    do app.UnhandledException.Add(fun e -> 
+            if (System.Diagnostics.Debugger.IsAttached) then
+                // An unhandled exception has occurred, break into the debugger
+                System.Diagnostics.Debugger.Break();
+     )
+
+    let rootFrame = new PhoneApplicationFrame();
+
+    do app.RootVisual <- rootFrame;
+
+    // Handle navigation failures
+    do rootFrame.NavigationFailed.Add(fun _ -> 
+        if (System.Diagnostics.Debugger.IsAttached) then
+            // A navigation has failed; break into the debugger
+            System.Diagnostics.Debugger.Break())
+
+    // Navigate to the main page 
+    do rootFrame.Navigate(new Uri("/WindowsPhoneApp;component/MainPage.xaml", UriKind.Relative)) |> ignore
+
+    // Required object that handles lifetime events for the application
+    let service = PhoneApplicationService()
+    // Code to execute when the application is launching (eg, from Start)
+    // This code will not execute when the application is reactivated
+    do service.Launching.Add(fun _ -> accelerometer.Start())
+    // Code to execute when the application is closing (eg, user hit Back)
+    // This code will not execute when the application is deactivated
+    do service.Closing.Add(fun _ -> accelerometer.Stop())
+    // Code to execute when the application is activated (brought to foreground)
+    // This code will not execute when the application is first launched
+    do service.Activated.Add(fun _ -> accelerometer.Start())
+    // Code to execute when the application is deactivated (sent to background)
+    // This code will not execute when the application is closing
+    do service.Deactivated.Add(fun _ -> accelerometer.Stop())
+
+    do app.ApplicationLifetimeObjects.Add(service) |> ignore
